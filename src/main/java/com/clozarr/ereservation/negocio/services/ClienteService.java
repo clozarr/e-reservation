61dd92deeefd9e57d8c459091cac365ca064487f
Carlos A Lozano A <clozanoarroyo@gmail.com>
2019-11-04 15:42:14 -0500

    Se configura el CRUD dentro del paquete service, se anota la clase con @Service y los métodos con @Transaccional

---
+++ src/main/java/com/clozarr/ereservation/negocio/services/ClienteService.java
@@ -0,0 +1,56 @@
+/**
+ * 
+ */
+package com.clozarr.ereservation.negocio.services;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.clozarr.ereservation.model.Cliente;
+import com.clozarr.ereservation.negocio.repository.ClienteRepository;
+
+/**
+ * @author carlos
+ *
+ */
+@Service
+@Transactional(readOnly = true) // cuando la anotación se coloca a nivel de clase, indica que todos los métodos que NO estén anotados, heredarán esta anotación. en este caso puntual, las operaciones de consulta solo tendrán transaccionalidad READONLY 
+public class ClienteService {
+	
+	private final ClienteRepository  clienteRepository;
+	
+	public ClienteService(ClienteRepository clienteRepository) {
+	
+		this.clienteRepository = clienteRepository;
+	}
+
+	/**
+	 * Operación para crear un cliente 
+	 */
+	@Transactional
+	public Cliente create(Cliente cliente) {
+		
+		return this.clienteRepository.save(cliente);
+	}
+	/**
+	 * Operación para actualizar un cliente 
+	 */
+	@Transactional
+	public Cliente update(Cliente cliente) {
+		
+		return this.clienteRepository.save(cliente);
+	}
+	/**
+	 * Operación para eliminar un cliente 
+	 */
+	@Transactional
+	public void delete(Cliente cliente) {
+		
+		this.clienteRepository.delete(cliente);
+	}
+	
+	public Cliente findByIdentification(String identificacionCliente) {
+		
+		return this.clienteRepository.findByIdentification(identificacionCliente);
+	}
+}
