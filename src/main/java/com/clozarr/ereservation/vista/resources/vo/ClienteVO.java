bcf5d33ecb0c2ec711280b22c8d28fc9aa308b11
Carlos A Lozano A <clozanoarroyo@gmail.com>
2019-11-04 23:58:29 -0500

    se congigura la dependencia y la clase utilitaria de swagger, Se crea el servicio de clientes, se crea el Objeto plano ClienteVO

---
+++ src/main/java/com/clozarr/ereservation/vista/resources/vo/ClienteVO.java
@@ -0,0 +1,16 @@
+package com.clozarr.ereservation.vista.resources.vo;
+
+import lombok.Data;
+
+@Data
+public class ClienteVO {
+
+	private String idCliente;
+	private String nombreCliente;
+	private String apellidoCliente;
+	private String identificacionCliente;
+	private String direccionCliente;
+	private String telefonoCliente;
+	private String emailCliente;
+	
+}
