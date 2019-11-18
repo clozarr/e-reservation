9fd7b85a34601f22c0eb297418eb6db7e53b1773
Carlos A Lozano (Trabajo) <=>
2019-11-17 19:36:11 -0500

    Creación de un login personalizado

--- src/main/java/com/clozarr/ereservation/util/BasicConfiguration.java
+++ src/main/java/com/clozarr/ereservation/util/BasicConfiguration.java
@@ -4,9 +4,13 @@
 package com.clozarr.ereservation.util;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 /**
  * @author carlos.lozano
@@ -18,12 +22,30 @@ public class BasicConfiguration extends WebSecurityConfigurerAdapter {
    
 	protected void configure(HttpSecurity http) throws Exception{
 		
-		http.authorizeRequests()
+		http.csrf().disable()
+		.authorizeRequests()
 		.anyRequest().authenticated()
 		.and()
-		.formLogin()
-		.and()
-		.httpBasic();
+		.formLogin().loginPage("/app/login").permitAll()
+		.failureUrl("/app/login?error=true")
+		.defaultSuccessUrl("/app/home");
 		
 	}
+	
+   @Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		
+		PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+		auth.inMemoryAuthentication()
+		.withUser("carlos")
+		.password(encoder.encode("platzi"))
+		.roles("USER");
+	}
+   
+   @Override
+   public void configure(WebSecurity web){
+	   
+	   web.ignoring().antMatchers("/resources/**","/static/**","/css/**","/js/**","/images/");
+	   
+   }
 }
